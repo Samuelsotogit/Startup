748543a1ac5e45d0fb220c4fcf9e25ca5b58e546
Samuel Soto <samuelsotointernational@gmail.com>
2024-02-27 22:45:30 -0700

    Update(HTML) Changed the navigation bar structure to div instead of li, set up the drop down menu button display, and set a single id for each 'save post' icon.

--- general.js
+++ general.js
@@ -15,12 +15,12 @@ function create_dropdown_menu() {
     const parentDiv = document.getElementsByClassName("nav-right")[0];
     const div = document.createElement("div");
     div.className = "dropdown";
-    div.innerHTML = `<button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    <span class="material-symbols-outlined">
+    div.innerHTML = `<button id="triple-bar-container" class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <span id="triple-bar" class="material-symbols-outlined">
         menu
         </span>
   </button>
-  <ul class="dropdown-menu">
+  <ul id="menu-symbol" class="dropdown-menu">
     <li><a class="dropdown-item" href="#">Notifications</a></li>
     <li><button id="log_out-button" onclick="log_out()"> Log out</button></li>
   </ul>`
