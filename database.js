648abf57c811b8aa51593b8f078c7810e687aa4f
Samuel Soto <samuelsotointernational@gmail.com>
2024-03-20 12:59:34 -0600

    Update: I required cookieparser, bcrypt, and my database on my index.js file. I also required additional things in the database.js file. I also began working on functions related to my startup on my database.js file.

--- database.js
+++ database.js
@@ -1,12 +1,15 @@
 const { MongoClient } = require('mongodb');
 const config = require('../dbConfig.json');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 async function main() {
   // Connect to the database cluster
   const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
   const client = new MongoClient(url);
   const db = client.db('rental');
-  const collection = db.collection('house');
+  const userCollection = db.collection('user');
+  const postsCollection = db.Collection('posts');
 
   // Test that you can connect to the database
   (async function testConnection() {
@@ -16,6 +19,32 @@ async function main() {
     console.log(`Unable to connect to database with ${url} because ${ex.message}`);
     process.exit(1);
   });
+
+
+  function getUser(email) {
+    return userCollection.findOne({ emial: email});
+  }
+
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token});
+  }
+
+  async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+    
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+
+    return user;
+  }
+
+  function addPost(post) {
+    postsCollection.insertOne(post);
+  }
 
   // Insert a document
   const house = {
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
+
+const authCookieName = 'token';
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
