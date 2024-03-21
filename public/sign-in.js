8f8da001ffbd3dd890aa03626cab09d5afb5ba43
Samuel Soto <samuelsotointernational@gmail.com>
2024-03-21 17:43:50 -0600

    Update(request) Created a post request to my backend from the frontend to request username id. Also removed password allocation from local storage.

--- public/sign-in.js
+++ public/sign-in.js
@@ -1,4 +1,4 @@
-function sign_in() {
+async function sign_in() {
     const nameEl = document.querySelector("#username");
     const emailEl = document.querySelector("#email");
     const passwordEl = document.querySelector("#password");
@@ -10,6 +10,23 @@ function sign_in() {
 
     localStorage.setItem('username', nameEl.value);
     localStorage.setItem('email', emailEl.value);
-    localStorage.setItem('Password', passwordEl.value);
-    window.location.href="main-feed.html";
+    // localStorage.setItem('Password', passwordEl.value);
+    try {
+        let user = {
+            username: nameEl.value,
+            email: emailEl.value,
+            password: passwordEl.value
+        }
+        let response = await fetch('/api/auth/create', {
+          method: 'POST',
+          headers: {'content-type': 'application/json'},
+          body: JSON.stringify(user),
+        });
+        user = await response.json();
+        window.location.href="main-feed.html";
+
+      } catch {
+        // If there was an error then just track scores locally
+        console.log('Unable to create user');
+      }
 }
