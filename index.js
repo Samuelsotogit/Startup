855ef90ee4fdf6cf5ad27885d31c2990888d1678
Samuel Soto <samuelsotointernational@gmail.com>
2024-03-21 17:39:26 -0600

    Update(Multiple Endpoints) Created an authentication/create user endpoint. Added a username parameter to 'createUser' call. Edited the login endpoints and post endpoints to use 'username' and 'post' as parameters instead of the simon parameters.

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
 
@@ -30,15 +33,15 @@ app.use((_req, res) => {
 });
 
 
-apiRouter.post('/post', (req, res) => {
-  let post = req.body
-  post.id = counter;
-  post.ratings = [];
-  counter++;
-  posts.push(post)
-  res.send(post)
-  // res.sendStatus(200)
-});
+// apiRouter.post('/post', (req, res) => {
+//   let post = req.body
+//   post.id = counter;
+//   post.ratings = [];
+//   counter++;
+//   posts.push(post)
+//   res.send(post)
+//   // res.sendStatus(200)
+// });
 
 apiRouter.put('/posts/:rating/:id', (req, res) => {
   updateRating(req.params.id,req.params.rating)
@@ -49,6 +52,89 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await DB.getUser(req.body.username)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.username, req.body.email, req.body.password);
+
+    // Set the cookie
+    setAuthCookie(res, user.token);
+
+    res.send({
+      id: user._id,
+    });
+  }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await DB.getUser(req.body.username);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      setAuthCookie(res, user.token);
+      res.send({ id: user._id });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+  const user = await DB.getUser(req.params.email);
+  if (user) {
+    const token = req?.cookies.token;
+    res.send({ email: user.email, authenticated: token === user.token });
+    return;
+  }
+  res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
+// GetScores
+secureApiRouter.get('/scores', async (req, res) => {
+  const scores = await DB.getHighScores();
+  res.send(scores);
+});
+
+// SubmitScore
+secureApiRouter.post('/post', async (req, res) => {
+  let post = req.body;
+  // const post = { ...req.body, ip: req.ip };
+  let updatedPost = await DB.addPost(post);
+  console.log(updatedPost);
+  res.send(updatedPost);
+});
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
 
 // JS
 
