c8fa9c3445f530c5c4b3ce8884a441244df887ba
Samuel Soto <samuelsotointernational@gmail.com>
2024-03-26 13:11:35 -0600

    Fixed(Authentication) Only existing users can login. I added an if statement to ensure that the request response was of type 200 to fixed bug. Got rid of the try catch block.

--- public/login.js
+++ public/login.js
@@ -1,28 +1,28 @@
+// figure out a way to not be able to login unless user has an email.
+
 async function login() {
-    const nameEl = document.querySelector("#username");
-    const passwordEl = document.querySelector("#password");
+  const nameEl = document.querySelector("#username");
+  const passwordEl = document.querySelector("#password");
 
-    if (nameEl.value.trim() === "" || passwordEl.value.trim() === "") {
-        alert("Please enter both username and password.");
-        return;
-    }
-    localStorage.setItem('userName', nameEl.value);
-    // localStorage.setItem('Password', passwordEl.value);
-      try {
-        let user = {
-            username: nameEl.value,
-            password: passwordEl.value
-        }
-        let response = await fetch('/api/auth/login', {
-          method: 'POST',
-          headers: {'content-type': 'application/json'},
-          body: JSON.stringify(user),
-        });
-        user = await response.json();
-        window.location.href="main-feed.html";
-
-      } catch {
-        // If there was an error then just track scores locally
-        console.log('Unable to find user');
-      }
+  if (nameEl.value.trim() === "" || passwordEl.value.trim() === "") {
+      alert("Please enter both username and password.");
+      return;
+  }
+  localStorage.setItem('userName', nameEl.value);
+  // localStorage.setItem('Password', passwordEl.value);
+  let user = {
+    username: nameEl.value,
+    password: passwordEl.value
+  }
+  // error happening here.
+  let response = await fetch('/api/auth/login', {
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    body: JSON.stringify(user),
+  });
+  if (response.ok) {
+    localStorage.setItem('userName', user.username);
+    window.location.href="main-feed.html";}
 }
+
+
